bec327db14d15cbbb413125c839ab1f77b0efbd8
trevinshu <trevinshu2008@gmail.com>
2021-02-15 00:36:40 -0700

    project finished

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  //Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  //Show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //Calculate Results
-function calculateResults(e) {
+function calculateResults() {
   //   console.log('calculating');
 
   //UI Variables
@@ -25,11 +35,23 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+    //Show Results
+    document.getElementById('results').style.display = 'block';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     // console.log('Please check your numbers');
+
+    //Hide Results
+    document.getElementById('results').style.display = 'none';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
     showError('Please check your numbers');
   }
-  e.preventDefault();
 }
 
 //Show Error
